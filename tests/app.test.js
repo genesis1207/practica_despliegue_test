80ef59434320248897e2496ef48a36fe1e7c2682
genesis1207 <106367610+genesis1207@users.noreply.github.com>
2026-07-07 16:59:55 -0500

    h1

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const express = require('express');
 const fs = require('fs');
-const app = require('../index'); // Necesitamos exportar `app` desde index.js
+const app = require('../index'); // Necesitamos una exportar `app` desde index.js
 
 describe('API de usuarios', () => {
   const testUser = { id: 'test123', name: 'Test User', email: 'test@example.com' };
